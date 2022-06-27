858feea17094703d60c90f5657deceab8ad88b9d
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-06-27 10:34:08 +0200

    Learn typescript - Catch error with interface

--- index.ts
+++ index.ts
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// Define the structerd of obj
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then ( ({data}) => {
-    console.log(data)
+    const {ID, Title, finished} = data as Todo;
+
+    console.log(`
+        The TODO with ID : ${ID}
+        Has a title of: ${Title}
+        and is finished? ${finished}
+    `)
 })
