062ca846c35dcf2b27ca5ec2252589fa05f8d8f2
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-06-27 15:30:02 +0200

    array object tuples

---
+++ annotations/array.ts
@@ -0,0 +1,14 @@
+const carMakers: string [] = ['ford', 'chevy', 'toyota']
+const dates = [ new Date(), new Date()];
+
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+]
+
+const myCar = carMakers.pop()
+
+const importantDate: (Date | string)[] = [new Date()]
+importantDate.push('2030-10-10')
+importantDate.push(new Date())
