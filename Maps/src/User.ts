ff4cbadec525b637bb2d0de0cd9faf71d92be681
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-07-19 17:45:08 +0200

    get google maps view

--- Maps/src/User.ts
+++ Maps/src/User.ts
@@ -1,11 +1,19 @@
+import faker from 'faker';
+
 class User {
+	constructor() {
+		this.name = faker.name.firstName();
+		this. location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude()),
+		}
+	};
+
     name: string;
     location: {
         lat: number;
         lng: number;
-    };
-
-    constructor() {
-        
     }
 }
+
+export default User;
